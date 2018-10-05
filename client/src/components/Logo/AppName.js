af22de8205ab90e166d341eb1a0680fce604d4d1
Dave Morris <davem226@yahoo.com>
2018-10-05 03:24:01 -0400

    Logo complete

--- client/src/components/Logo/AppName.js
+++ client/src/components/Logo/AppName.js
@@ -1,4 +1,14 @@
 import React from 'react';
 import "./Logo.css";
 
-export const AppName = () => <div className="app-name">myNews</div>
+export const AppName = () => (
+    <div>
+        <div className="logo">
+            <div className="sky-bar"></div>
+            <div className="orange-bar"></div>
+            <div className="yellow-bar"></div>
+            <div className="sun"></div>
+            <div className="app-name">myNews</div>
+        </div>
+    </div>
+)
