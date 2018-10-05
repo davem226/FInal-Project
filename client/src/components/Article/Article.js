f95746083ae31573146fb2b34a302b0a62a0df82
Dave Morris <davem226@yahoo.com>
2018-10-05 00:41:30 -0400

    Article styling updated

--- client/src/components/Article/Article.js
+++ client/src/components/Article/Article.js
@@ -2,11 +2,12 @@ import React from "react";
 
 const Article = (props) => (
     <div className="article">
+        <div className="color-bar"></div>
         <a href={props.link} target="_blank" alt="article">
             <h2>{props.title}</h2>
         </a>
-        <p>{props.preview}</p>
         <div className="source">{props.source}</div>
+        <p>{props.preview}</p>
         {props.children}
     </div>
 );
