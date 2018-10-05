5e7336664a019495371c96f6aa9c2ef6ac9b1dea
Dave Morris <davem226@yahoo.com>
2018-10-04 21:46:37 -0400

    Article styling updated

--- client/src/components/Article/Article.js
+++ client/src/components/Article/Article.js
@@ -3,7 +3,7 @@ import React from "react";
 const Article = (props) => (
     <div className="article">
         <a href={props.link} target="_blank" alt="article">
-            <h1>{props.title}</h1>
+            <h2>{props.title}</h2>
         </a>
         <p>{props.preview}</p>
         <div className="source">{props.source}</div>
