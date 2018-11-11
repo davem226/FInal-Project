8a9cdaeec4bd3e6f69020e8652f794391ae102e2
Dave Morris <davem226@yahoo.com>
2018-11-11 12:10:53 -0500

    added Microsoft Azure Sentiment Analysis to logistic regression

--- client/src/utils/logreg.js
+++ client/src/utils/logreg.js
@@ -1,26 +1,53 @@
-// import jsregression from "js-regression";
+import API from "./api";
 
-// // === Create the linear regression === //
-// const logistic = new jsregression.LogisticRegression({
-//     alpha: 0.001,
-//     iterations: 1000,
-//     lambda: 0.0
-// });
-// module.exports = logistic;
+export default class LogReg {
+    // Get articles data from db
+    getData = uid => {
+        API.getArticles(uid)
+            .then(res => {
+                return this.processData(res.data);
+            })
+            .catch(err => console.log(err));
+    };
+    // Make data analyzable
+    processData = data => {
+        // Run sentiment analyses (Microsoft Azure)
+        const sentimentTitle = this.sentimentAnalysis(data, "title");
+        const sentimentPreview = this.sentimentAnalysis(data, "preview");
 
-// // can also use default configuration: const logistic = new jsregression.LogisticRegression(); 
-
-// // === Train the logistic regression === //
-// const model = logistic.fit(trainingData);
-
-// // === Print the trained model === //
-// console.log(model);
-
-// // === Testing the trained logistic regression === //
-// for (let i = 0; i < testingData.length; ++i) {
-//     const probabilityOfSpeciesBeingIrisVirginica = logistic.transform(testingData[i]);
-//     const predicted = logistic.transform(testingData[i]) >= logistic.threshold ? 1 : 0;
-//     console.log("actual: " + testingData[i][4] + " probability of being Iris-virginica: " + probabilityOfSpeciesBeingIrisVirginica);
-//     console.log("actual: " + testingData[i][4] + " predicted: " + predicted);
-// }
-
+        const processedData = [];
+        const dummyCols = this.dummyCode(data, "source");
+        for (let row of data) {
+            const datum = {};
+            datum.choice = row.choice === "yes" ? 1 : 0;
+            for (let i in dummyCols) {
+                datum[`source_${dummyCols[i]}`] = row.source === dummyCols[i] ? 1 : 0;
+            }
+            // console.log(datum);
+            processedData.push(datum);
+        }
+        console.log(processedData);
+        return processedData;
+    };
+    sentimentAnalysis = (data, column) =>{
+        const documents = data.map(row => {
+            return { language: "en", id: row.id, text: row[column] };
+        });
+        API.sentiment({ documents })
+            .then(data => {
+                return data.map(row => {
+                    return { id: row.id, score: row.score };
+                });
+            }).catch(err => console.log(err));
+    };
+    dummyCode = (data, columnToDummy) => {
+        let dummyCols = [];
+        // Create array of all categories in a categorical data column
+        for (let row of data) {
+            if (!dummyCols.includes(row[columnToDummy])) {
+                dummyCols = dummyCols.concat(row[columnToDummy]);
+            }
+        }
+        return dummyCols;
+    }
+}
