72e261790c043a15a6ce51eda53c3288bc42a4d3
Dave Morris <davem226@yahoo.com>
2018-11-21 22:06:53 -0500

    Completed .fit method; not coded very functionally

--- client/src/utils/logreg.js
+++ client/src/utils/logreg.js
@@ -9,16 +9,26 @@ export default class LogReg {
             }).catch(err => console.log(err));
     };
     // Estimate parameter values of logistic regression model
-    fit = (data,η) => {
-        // Define sigma function
-        const σ = z => 1/(1+Math.exp(-z));
-        // Initialize parameters to zero
-        let θ = Object.keys(data[0]).filter(col=>col!=="choice").map(col=>0);
-        data.map(row=>{
-            const x = Object.keys(row).filter(col=>col!=="choice").map(col=>row[col]);
-            const z = x.map((val,i)=>val*θ[i]);
-            const partial = 
-        })
+    fit = (data, repeat) => {
+        const σ = z => 1 / (1 + Math.exp(-z));
+        const η = 0.05;
+        // Initialize parameters to zero, including intercept
+        let θ = [0, ...Object.keys(data[0]).filter(col => col !== "choice").map(col => 0)];
+        // Repeat many times
+        while (repeat) {
+            const gradients = θ.slice();
+            data.map(row => {
+                // The intercept x-value is set to 1
+                const x = [1, ...Object.keys(row).filter(col => col !== "choice").map(col => row[col])];
+                const z = x.reduce((acc, val, i) => acc + val * θ[i]);
+                gradients.map((grad, i) => {
+                    gradients[i] = grad + (row.choice - σ(z)) * x[i];
+                })
+            })
+            θ = θ.map((x, i) => x + η * gradients[i]);
+            repeat--;
+        }
+        return θ;
     };
     // Make data analyzable
     processData = data => {
@@ -32,12 +42,12 @@ export default class LogReg {
         // Concatenate data
         // Might have to async-await this with the sentimentAnalysis calls
         return (
-            data.map(row=>{
+            data.map(row => {
                 return {
                     choice: row.choice === "yes" ? 1 : 0,
-                    sentimentTitle: sentimentTitle.find(obj=>obj.id===row.id).score,
-                    sentimentPreview: sentimentPreview.find(obj=>obj.id===row.id).score,
-                    ...dummySource.find(obj=>obj.id===row.id)
+                    sentimentTitle: sentimentTitle.find(obj => obj.id === row.id).score,
+                    sentimentPreview: sentimentPreview.find(obj => obj.id === row.id).score,
+                    ...dummySource.find(obj => obj.id === row.id)
                 };
             })
         );
@@ -53,7 +63,7 @@ export default class LogReg {
     };
     // Outputs array of objects with dummy coded categorical variables
     dummyCode = (data, column) => {
-        const categories = data.filter((row,i)=>data.indexOf(row[column])===i);
+        const categories = data.filter((row, i) => data.indexOf(row[column]) === i);
         return (
             data.map(row => {
                 const dummyData = { id: row.id };
